f5fc1d74769dd8e1559f7b848d1b870fa65244b4
zhoudawei <zdw0607@gmail.com>
2022-07-02 17:01:43 +0800

    api service

---
+++ user/rpc/rpc.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/zhovdawei/gozero-micro-service/user/rpc/internal/config"
+	"github.com/zhovdawei/gozero-micro-service/user/rpc/internal/server"
+	"github.com/zhovdawei/gozero-micro-service/user/rpc/internal/svc"
+	"github.com/zhovdawei/gozero-micro-service/user/rpc/rpc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/rpc.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+	svr := server.NewRpcServer(ctx)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		rpc.RegisterRpcServer(grpcServer, svr)
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
